069d12c5addd203cd4075be32f507ff025c47a40
marcospedroche05 <marcospedroche62@gmail.com>
2026-02-09 14:22:53 +0100

    puch

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        //EL CONSTRUCTOR RECIBIRA SIEMPRE LAS OPCIONES PARA ESTE CONTEXTO
+        //La clase que recibe es DBContextOptions<Context>
+        //ESTAS OPTIONS DEBEMOS ENVIARLAS A LA CLASE BASE/SUPER
+        //DEL DBCONTEXT
+        public HospitalContext(DbContextOptions<HospitalContext> options):base(options)
+        {}
+
+        //DEBEMOS TENER UNA COLECCION POR CADA MODEL
+        //DICHA COLECCION DEBE SER DE TIPO DbSet<T>
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
---
+++ MvcCoreEF/Models/Hospital.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcCoreEF.Models
+{
+    //MEDIANTE DECORACIONES DE MAPPING VAMOS 
+    //INDICANDO A QUE TABLA/VIEW APUNTA
+    [Table("HOSPITAL")]
+    public class Hospital
+    {
+        [Key]
+        [Column("HOPSITAL_COD")]
+        public int IdHospital { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("DIRECCION")]
+        public string Direccion { get; set; }
+        [Column("TELEFONO")]
+        public string Telefono { get; set; }
+        [Column("NUM_CAMA")]
+        public int Camas { get; set; }
+    }
+}
